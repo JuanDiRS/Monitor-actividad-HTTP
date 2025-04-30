f43afe9661c478b6ad3a1d7233e23f8d4e0748a3
Juan Diego Rubiano <juandirusaenz@gmail.com>
2025-04-30 14:17:38 -0500

    Add files via upload

---
+++ build/CMakeFiles/ProyectoLu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoLu.dir/ProyectoLu.cpp.o"
+  "CMakeFiles/ProyectoLu.dir/ProyectoLu.cpp.o.d"
+  "ProyectoLu"
+  "ProyectoLu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoLu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
